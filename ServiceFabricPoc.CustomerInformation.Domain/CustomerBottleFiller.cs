276e4b77dc9a6a5ef71aba020eb7d59cb5506344
bkotvis <bkotvis@USUAPNB42194>
2017-02-10 14:09:43 -0600

    added customer information service

---
+++ ServiceFabricPoc.CustomerInformation.Domain/CustomerBottleFiller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceFabricPoc.CustomerInformation.Domain
+{
+    [DataContract]
+    public class CustomerBottleFiller
+    {
+        [DataMember]
+        public string Id { get; set; }
+        [DataMember]
+        public string LocationDescription { get; set; }
+        [DataMember]
+        public string SerialNumber { get; set; }
+        [DataMember]
+        public string  Type { get; set; }
+        [DataMember]
+        public string  ProductFamily { get; set; }
+    }
+}
---
+++ ServiceFabricPoc.CustomerInformation.Domain/CustomerInformation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceFabricPoc.CustomerInformation.Domain
+{
+    [DataContract]
+    public class CustomerInformation
+    {
+        [DataMember]
+        public List<CustomerBottleFiller> BottleFillers { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string MainLocationId { get; set; }
+    }
+}
